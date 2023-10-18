0a2308d4e7253d148c153083f17ff5ab62a668e3
Gyeolee <gyeoleeeeeee@gmail.com>
2023-10-18 15:46:19 +0900

    [UPDATE] WidgetCenter 관련 코드 추가

--- WidgetSample/WidgetSample/ContentView.swift
+++ WidgetSample/WidgetSample/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                WidgetCenter.shared.reloadTimelines(ofKind: "MyWidget")
+            } label: {
+                Text("Reload Timelines")
+            }
+            
+            Button {
+                WidgetCenter.shared.reloadAllTimelines()
+            } label: {
+                Text("Reload All Timelines")
+            }
         }
         .padding()
+        .onAppear {
+            WidgetCenter.shared.getCurrentConfigurations { result in
+                switch result {
+                case .success(let widgets):
+                    widgets.forEach {
+                        print("kind: \($0.kind), family: \($0.family)")
+                    }
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
