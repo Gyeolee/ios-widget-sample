13949bfffed6285e67d29a57abf45573680521ea
Gyeolee <gyeoleeeeeee@gmail.com>
2023-10-18 14:00:26 +0900

    [ADD] 프로젝트 생성

---
+++ WidgetSample/WidgetSample/WidgetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSampleApp.swift
+//  WidgetSample
+//
+//  Created by Hangyeol on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
